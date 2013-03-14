7f5c60dfd09ae53d718e80324b4ae2b2983427b9
Per Andre Omsveen <Per@dhcp-005044.wlan.ntnu.no>
2013-03-14 15:09:29 +0100

    ny login

--- src/no/ntnu/fp/gui/Login.java
+++ src/no/ntnu/fp/gui/Login.java
@@ -5,6 +5,9 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class Login extends JPanel {
 	private JTextField textField;
@@ -16,26 +19,37 @@ public class Login extends JPanel {
 	public Login() {
 		setLayout(null);
 		
-		JLabel lblEmail = new JLabel("email:");
-		lblEmail.setBounds(214, 267, 61, 16);
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblEmail.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblEmail.setBounds(321, 341, 61, 16);
 		add(lblEmail);
 		
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(190, 295, 61, 16);
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPassword.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblPassword.setBounds(296, 375, 86, 16);
 		add(lblPassword);
 		
 		textField = new JTextField();
-		textField.setBounds(280, 261, 134, 28);
+		textField.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+		textField.setBounds(386, 335, 196, 28);
 		add(textField);
 		textField.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(280, 289, 134, 28);
+		passwordField.setBounds(386, 369, 196, 28);
 		add(passwordField);
 		
 		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(280, 329, 117, 29);
+		btnLogin.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		btnLogin.setBounds(386, 407, 94, 29);
 		add(btnLogin);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/images/xcal.png")));
+		lblNewLabel.setBounds(314, 125, 303, 155);
+		add(lblNewLabel);
 
 	}
 }
---
+++ src/no/ntnu/fp/gui/Notification.java
@@ -0,0 +1,95 @@
+package no.ntnu.fp.gui;
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.DropMode;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+
+
+public class Notification extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Notification frame = new Notification();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Notification() {
+		setBounds(100, 100, 562, 439);
+		getContentPane().setLayout(null);
+		
+		JLabel lblYouAreInvited = new JLabel("You are invited to a meeting");
+		lblYouAreInvited.setFont(new Font("Helvetica", Font.BOLD, 14));
+		lblYouAreInvited.setBounds(57, 26, 202, 29);
+		getContentPane().add(lblYouAreInvited);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Notification.class.getResource("/images/1362768365_Group_Meeting_Light.png")));
+		lblNewLabel.setBounds(341, 34, 142, 133);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblInvitertAv = new JLabel("Invited by:");
+		lblInvitertAv.setFont(new Font("Helvetica", Font.PLAIN, 14));
+		lblInvitertAv.setBounds(67, 55, 73, 16);
+		getContentPane().add(lblInvitertAv);
+		
+		JLabel lblKari = new JLabel("Kari");
+		lblKari.setFont(new Font("Helvetica", Font.BOLD, 14));
+		lblKari.setBounds(137, 55, 188, 16);
+		getContentPane().add(lblKari);
+		
+		JLabel lblMeatingSubject = new JLabel("Coffie talk meeting");
+		lblMeatingSubject.setFont(new Font("Helvetica", Font.BOLD, 18));
+		lblMeatingSubject.setBounds(57, 80, 228, 44);
+		getContentPane().add(lblMeatingSubject);
+		
+		JLabel lblNewLabel_1 = new JLabel(" Time: 12:30 - 14:30");
+		lblNewLabel_1.setFont(new Font("Helvetica", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(63, 117, 218, 16);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Date:  04.April.2013");
+		lblNewLabel_2.setBounds(67, 136, 132, 16);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblDescription = new JLabel("Description:");
+		lblDescription.setFont(new Font("Helvetica", Font.BOLD, 14));
+		lblDescription.setBounds(57, 174, 158, 16);
+		getContentPane().add(lblDescription);
+		
+		JButton btnNewButton = new JButton("Accept");
+		btnNewButton.setBounds(62, 325, 117, 29);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Decline");
+		btnNewButton_1.setBounds(185, 325, 117, 29);
+		getContentPane().add(btnNewButton_1);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setFont(new Font("Helvetica", Font.PLAIN, 14));
+		textPane.setBounds(67, 202, 377, 114);
+		getContentPane().add(textPane);
+
+	}
+}
