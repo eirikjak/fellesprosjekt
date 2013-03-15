c2594a9b99257ddeeb7476b6f34e27dd53098c68
Per Andre Omsveen <Per@dhcp-005017.wlan.ntnu.no>
2013-03-15 11:18:33 +0100

    ny mainpage

--- src/xcal/gui/Mainpage.java
+++ src/xcal/gui/Mainpage.java
@@ -23,7 +23,7 @@ public class Mainpage extends JPanel {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.setBounds(35, 37, 279, 95);
+		panel.setBounds(32, 34, 279, 95);
 		add(panel);
 		panel.setLayout(null);
 		
@@ -42,9 +42,64 @@ public class Mainpage extends JPanel {
 		panel.add(btnNewButton);
 		btnNewButton.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(143, 179, 206));
+		panel_2.setBounds(40, 256, 922, 27);
+		add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel label_1 = new JLabel("DATO");
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_1.setBounds(20, 4, 90, 19);
+		panel_2.add(label_1);
+		
+		JLabel label_2 = new JLabel("DATO");
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		label_2.setForeground(Color.WHITE);
+		label_2.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_2.setBounds(285, 4, 90, 19);
+		panel_2.add(label_2);
+		
+		JLabel label_3 = new JLabel("DATO");
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		label_3.setForeground(Color.WHITE);
+		label_3.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_3.setBounds(150, 4, 90, 19);
+		panel_2.add(label_3);
+		
+		JLabel label_4 = new JLabel("DATO");
+		label_4.setHorizontalAlignment(SwingConstants.CENTER);
+		label_4.setForeground(Color.WHITE);
+		label_4.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_4.setBounds(416, 4, 90, 19);
+		panel_2.add(label_4);
+		
+		JLabel label_5 = new JLabel("DATO");
+		label_5.setHorizontalAlignment(SwingConstants.CENTER);
+		label_5.setForeground(Color.WHITE);
+		label_5.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_5.setBounds(549, 4, 90, 19);
+		panel_2.add(label_5);
+		
+		JLabel label_6 = new JLabel("DATO");
+		label_6.setHorizontalAlignment(SwingConstants.CENTER);
+		label_6.setForeground(Color.WHITE);
+		label_6.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_6.setBounds(681, 4, 90, 19);
+		panel_2.add(label_6);
+		
+		JLabel label_7 = new JLabel("DATO");
+		label_7.setHorizontalAlignment(SwingConstants.CENTER);
+		label_7.setForeground(Color.WHITE);
+		label_7.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		label_7.setBounds(814, 4, 90, 19);
+		panel_2.add(label_7);
+		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.setBounds(35, 169, 940, 493);
+		panel_1.setBounds(32, 166, 940, 493);
 		add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -70,33 +125,33 @@ public class Mainpage extends JPanel {
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.BOLD, 28));
 		
-		JList list = new JList();
-		list.setBounds(8, 122, 126, 365);
-		panel_1.add(list);
-		
 		JList list_1 = new JList();
-		list_1.setBounds(141, 122, 126, 365);
+		list_1.setBounds(8, 122, 126, 365);
 		panel_1.add(list_1);
 		
 		JList list_2 = new JList();
-		list_2.setBounds(274, 122, 126, 365);
+		list_2.setBounds(141, 122, 126, 365);
 		panel_1.add(list_2);
 		
 		JList list_3 = new JList();
-		list_3.setBounds(540, 122, 126, 365);
+		list_3.setBounds(274, 122, 126, 365);
 		panel_1.add(list_3);
 		
 		JList list_4 = new JList();
-		list_4.setBounds(806, 122, 126, 365);
+		list_4.setBounds(540, 122, 126, 365);
 		panel_1.add(list_4);
 		
 		JList list_5 = new JList();
-		list_5.setBounds(673, 122, 126, 365);
+		list_5.setBounds(806, 122, 126, 365);
 		panel_1.add(list_5);
 		
 		JList list_6 = new JList();
-		list_6.setBounds(407, 122, 126, 365);
+		list_6.setBounds(673, 122, 126, 365);
 		panel_1.add(list_6);
+		
+		JList list_7 = new JList();
+		list_7.setBounds(407, 122, 126, 365);
+		panel_1.add(list_7);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
