22cd35b14b9e0efc15707c2acac4784271810373
nemanjaa <nemanjaa@stud.ntnu.no>
2013-03-15 13:12:18 +0100

    room query

---
+++ src/xcal/server/query/RoomQ.java
@@ -0,0 +1,21 @@
+package xcal.server.query;
+
+import xcal.model.*;
+
+public class RoomQ {
+	
+	public void createRoom(Room room){
+		
+	}
+	
+	public void updateRoom(Room room){
+		
+	}
+	
+	public Room selectRoom(int romId){
+		return null;
+	}
+	
+	
+
+}
