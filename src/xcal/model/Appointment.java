6913cb004f66019983ea40667a8f673cfc472f47
Christer Lund <christer.lund@gmail.com>
2013-03-18 15:42:43 +0100

    added selectAppointmentFromDate

--- src/xcal/model/Appointment.java
+++ src/xcal/model/Appointment.java
@@ -7,50 +7,38 @@ package xcal.model;
 
 import java.sql.Timestamp;
 
-import org.joda.time.DateTime;
-
 public class Appointment
 {
 	private int appId;
 	
 	private String location;
-	private DateTime from_time;
-	private DateTime to_time;
+	private Timestamp from_time;
+	private Timestamp to_time;
 	private String name;
 	private String description;
-	private String title;
-	private Employee leader;
 	
-	public Appointment(DateTime start,DateTime end,String title,String description,Employee leader,String place)
+	public Appointment()
 	{
-		this.from_time = start;
-		this.to_time = end;
-		this.title = title;
-		this.description = description;
-		this.leader = leader;
-		this.location = place;
-		
-		
+		location=null;
 	}
 	
 
 	
-	public void setFromTime(DateTime from){from_time=from;}
-	public void setToTime(DateTime to){to_time=to;}
-	public void setLocation(int location){this.location=location;}
+	public void setFromTime(Timestamp from){from_time=from;}
+	public void setToTime(Timestamp to){to_time=to;}
+	public void setLocation(String location){this.location=location;}
 	public void setName(String name){this.name=name;}
 	public void setDescription(String desc){this.description=desc;}
 
 	
-	public DateTime getFromTime(){return from_time;}
-	public DateTime getToTime(){return to_time;}
-	public int getLocation(){return location;}
+	public Timestamp getFromTime(){return from_time;}
+	public Timestamp getToTime(){return to_time;}
+	public String getLocation(){return location;}
 	public String getName(){return name;}
 	public String getDescription(){return description;}
 	public int getAppId() {
 		return appId;
 	}
-	public Employee getLeader(){return leader;}
 
 	
 	
--- src/xcal/server/query/AppointmentsQ.java
+++ src/xcal/server/query/AppointmentsQ.java
@@ -25,7 +25,7 @@ public class AppointmentsQ
 		// TODO Auto-generated constructor stub
 	}
 
-	public static Appointment createAppointment(Appointment app){
+	public  Appointment createAppointment(Appointment app){
 		
 		synchronized (connection) {
 		DateTimeFormatter format = DateTimeFormat.forPattern("Y-M-d H:m:s");
@@ -38,8 +38,35 @@ public class AppointmentsQ
 	
 	}
 	
-	
-	public static Appointment selectAppointment(int AppointmentId)
+	public Appointment selectAppointmentsFromDate (Timestamp startDate, Timestamp endDate) throws SQLException{
+		synchronized (connection) {
+			String sql = "select * from Appontment where date >= ["+startDate+"] and end_date <= ["+endDate+"];";
+			ResultSet resultSet = statement.executeQuery(sql);
+        	resultSet.last();
+        	
+        	if(resultSet.getString("room").isEmpty())//appointment doesn't contain room
+  		   {
+  			   Appointment app=new Appointment();
+  			  //app.setLocation(result.getString("Location"));
+  			   app.setDescription(resultSet.getString("description"));
+  			   //app.setName(result.getString("name"));
+  			   app.setFromTime(resultSet.getTimestamp("start_date"));
+  			   app.setToTime(resultSet.getTimestamp("end_date"));
+  			   return app;
+  		   }
+  		   
+  		   Appointment meeting=new Meeting();
+  		   meeting.setDescription(resultSet.getString("description"));
+  		   meeting.setFromTime(resultSet.getTimestamp("start_date"));
+  		   meeting.setToTime(resultSet.getTimestamp("end_date"));
+  		   
+  		   return meeting;   		   
+  		   
+
+  	   } 
+			
+	}
+	public  Appointment selectAppointment(int AppointmentId)
 	{
 		synchronized (connection) {
 		String query="select * from Appointment where id='"+AppointmentId+"'";
@@ -113,6 +140,8 @@ public class AppointmentsQ
 	
 	public static Meeting selectMeeting(int appId){
 		synchronized (connection) {
+			
+			
 		return null;
 		}
 		
